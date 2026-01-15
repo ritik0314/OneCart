90c783e2cf282b7848e632a4618ca5410cb37650
Your Name <rsbaghel9314@gmail.com>
2026-01-15 15:45:02 +0530

    Apply postinstall change

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
+<<<<<<< Updated upstream
     let serverUrl = "https://onecart-backend11.onrender.com"
+=======
+    let serverUrl = "https://onecart-copy-1.onrender.com"
+>>>>>>> Stashed changes
 
     let value = {
        serverUrl
