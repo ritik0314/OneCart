7c0639e9d37818c352b5dbb92dfe1bd07a4c6f8b
Ritik Singh <rsbaghel9314@gmail.com>
2026-01-15 16:07:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://onecart12.onrender.com" , "http://localhost:5174"],
+ origin:["https://onecart12.onrender.com" , "https://onecart-admin1-5jwv.onrender.com"],
  credentials:true
 }))
 
